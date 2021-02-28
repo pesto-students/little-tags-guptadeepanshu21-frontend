dd31f863ce81ace9d21ceaa91d26cdf3e66327f6
Abhishek Singh <writetoabhishek07@gmail.com>
2021-03-01 03:55:55 +0530

    complete Carousel

--- little-tags/src/App.js
+++ little-tags/src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
-
+import Carousel from './components/Carousel/Carousel'
 
 function App() {
   return (
     <div className="main">      
         <NavBar />
-       
+        <Carousel/>
     </div>
   );
 }
