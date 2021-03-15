c6f7cb9b7f718b6c65926ed2e23223ef792bcdbc
Abhishek Singh <writetoabhishek07@gmail.com>
2021-03-15 13:39:47 +0530

    minor fixes firebase and product list

--- little-tags/src/components/Product-List/ProductList.js
+++ little-tags/src/components/Product-List/ProductList.js
@@ -3,10 +3,14 @@ import { Container, Row, Col,Breadcrumb, Card, CardColumns, Dropdown, DropdownBu
 import items from './ProductData'
 import Filters from '../Filter/Filters'
 import './ProductList.css'
+import { ToastContainer } from 'react-toastify';
 
 function ProductList() {
     return (
         <div className="wrapper">
+              <div className="home__toast-container">
+                    <ToastContainer />
+                </div>
             <Container>
             <Breadcrumb className="large">
             <Breadcrumb.Item href="">Home</Breadcrumb.Item>
@@ -34,25 +38,62 @@ function ProductList() {
             
             <Container className="mx-5">
             <CardColumns>
-              {items.value.map((value)=>(
-                <Card style={{ width: '24rem',height:"20%" }} className="mx-2 my-5">
-                <Card.Img variant="top" src={value.images}/>
-                <Card.Body>
-                  <Card.Title className="font-weight-bold ">{value.name}</Card.Title>
-                  
-                  <Row>
-                  <Card.Text className = "price">
-                  Rs. {value.mrp}
-                  </Card.Text>
-                  <ion-icon style = {{cursor: 'pointer'}} name="cart"></ion-icon>   
-                  </Row>
-                 
-                </Card.Body>
-              </Card>
-
-    ))
-
-              }
+            <Product
+                    id={2087285}
+                    title="Men Solid Bombet Jacket"
+                    price={7500}
+                    rating={4.5}
+                    image="https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/5453186/2018/10/30/ef1d9ca6-156b-4cf6-a52d-1e7f58b51b8b1540888118150-Roadster-Men-Jackets-9141540888117961-1.jpg"
+                />
+                <Product
+                    id={9137517}
+                    title="Women Solid Denim Jacket"
+                    price={3199}
+                    rating={4.5}
+                    image="https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/10604552/2019/9/12/fd347043-b506-4fc8-9997-ae6f939c73081568291650944-SASSAFRAS-Women-Jackets-8211568291648962-1.jpg"
+                />
+                <Product
+                    id={9137527}
+                    title="Men Reversible Bomber Jacket"
+                    price={7250}
+                    rating={5}
+                    image="https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/10646154/2019/11/27/d9ead1c6-cfb4-4313-9426-12e48d1da3121574859444519-Allen-Solly-Men-Mustard-Yellow--Black-Solid-Reversible-Bombe-1.jpg"
+                />
+                <Product
+                    id={9137527}
+                    title="Men Active Bomber Jacket"
+                    price={7250}
+                    rating={3.5}
+                    image="https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/4453297/2019/12/2/77b5d9e0-8d65-4a36-94e7-290ed271db6b1575267599079-HRX-by-Hrithik-Roshan-Men-Olive-Green-Solid-Active-Bomber-Ja-1.jpg"
+                />
+                <Product
+                    id={2087285}
+                    title="Essential White Cheater Jacket"
+                    price={7500}
+                    rating={3.5}
+                    image="https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/11442936/2020/9/1/47f08faa-8e57-472b-acaf-2c437bec5ebb1598959318825-Puma-Women-Jackets-391598959316816-1.jpg"
+                />
+                <Product
+                    id={9137517}
+                    title="Men Washed Denim Jacket"
+                    price={3199}
+                    rating={4.0}
+                    image="https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/10300923/2019/9/25/685704ff-e7bb-4d94-bad2-541dc25d14ac1569400045801-HERENOW-Men-Jackets-8751569400044582-1.jpg"
+                />
+                <Product
+                    id={9137517}
+                    title="Women Solid Barka"
+                    price={3999}
+                    rating={4.0}
+                    image="https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/5709433/2018/9/6/f8edfec2-d665-4dec-8bd0-1c549d9b2c051536228338868-Chemistry-Women-Brown-Solid-Parka-7841536228337271-1.jpg"
+                />
+                <Product
+                    id={9137517}
+                    title="Men Solid Bomber Jacket"
+                    price={3199}
+                    rating={4.0}
+                    image="https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/9564737/2019/11/7/e9e18815-c96a-4db2-b47d-5bf4995fe3081573103446301-Roadster-Men-Jackets-3391573103444243-1.jpg"
+                />
             </CardColumns>
             </Container>
         </Container>
--- little-tags/src/firebase/firebaseConfig.js
+++ little-tags/src/firebase/firebaseConfig.js
@@ -2,7 +2,7 @@ import firebase from "firebase/app"
 import 'firebase/firestore'
 import 'firebase/auth'
 
-const firebaseApp = {
+const firebaseApp = firebase.initializeApp( {
     apiKey: "AIzaSyAELQy2WpAMNkMyLnhd9idpAgPCPKg_y2Q",
     authDomain: "clothify-5dda0.firebaseapp.com",
     databaseURL: "https://clothify-5dda0-default-rtdb.firebaseio.com",
@@ -11,7 +11,7 @@ const firebaseApp = {
     messagingSenderId: "10636985651",
     appId: "1:10636985651:web:36ce0fc8b829d4273aaa46",
     measurementId: "G-SKY8GK2898"
-  };
+  });
 
 const db = firebaseApp.firestore()
 const auth = firebase.auth()
