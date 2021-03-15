728e7e6f6bf85b1ac5327e0cec60036db047af78
Abhishek Singh <writetoabhishek07@gmail.com>
2021-03-15 13:30:34 +0530

    setup firebase

---
+++ little-tags/src/firebase/firebaseConfig.js
@@ -0,0 +1,20 @@
+import firebase from "firebase/app"
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const firebaseApp = {
+    apiKey: "AIzaSyAELQy2WpAMNkMyLnhd9idpAgPCPKg_y2Q",
+    authDomain: "clothify-5dda0.firebaseapp.com",
+    databaseURL: "https://clothify-5dda0-default-rtdb.firebaseio.com",
+    projectId: "clothify-5dda0",
+    storageBucket: "clothify-5dda0.appspot.com",
+    messagingSenderId: "10636985651",
+    appId: "1:10636985651:web:36ce0fc8b829d4273aaa46",
+    measurementId: "G-SKY8GK2898"
+  };
+
+const db = firebaseApp.firestore()
+const auth = firebase.auth()
+var provider = new firebase.auth.GoogleAuthProvider();
+
+export { db, auth, provider }
